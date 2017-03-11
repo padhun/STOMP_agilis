8b83f90602017e81f4295d0a96d51b91d8813bcb
David Balla <szefoka@gmail.com>
2017-03-11 22:34:52 +0100

    Decoder decode function implemented

--- src/Decode.py
+++ src/Decode.py
@@ -1,10 +1,22 @@
 
-from Frames import *
+import Frames
 
 class Decoder(object):
 
     def __init__(self):
         pass
 
+    def get_frame_class(self,command):
+        frame_class = Frames.FRAMES.get(command)
+        if frame_class is None:
+            raise Exception('There is no frame type: ' + command)
+        return frame_class
+
     def decode(self,message):
-        pass
+        command, headers, msg = message.split('\n\n')[0].split()[0], \
+                                message.split('\n\n')[0].split()[1:], \
+                                message.split('\n\n')[1]
+        frame_class = self.get_frame_class(command)
+        frame_args = dict(item.split(":") for item in headers)
+        frame = frame_class(**frame_args)
+        return frame

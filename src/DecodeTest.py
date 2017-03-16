3252a1e0f5b2991179d3fabe66f34a19f7cd85c9
David Balla <szefoka@gmail.com>
2017-03-16 19:32:19 +0100

    Send and Connect frame tests

--- src/DecodeTest.py
+++ src/DecodeTest.py
@@ -21,11 +21,18 @@ class TestDecoder(unittest.TestCase):
         msg = "CONNECT\naccept-version:1.2\nhost:localhost\n\n\x00"
         self.assertEquals(self.decoder.decode(msg).__dict__, testFrame.__dict__)
 
+    def test_decoder_decode_connect_missing_req_header(self):
+        msg = "CONNECT\nhost:localhost\n\n\x00"
+        self.assertRaises(Exception, self.decoder.decode(msg))
+
     def test_decoder_decode_send(self):
-        testFrame = Frames.CONNECT(**{"accept-version":"1.2", "host":"localhost", "msg":"hello queue a"})
-        msg = "SEND\naccept-version:1.2\nhost:localhost\n\nhello queue a\x00"
+        testFrame = Frames.SEND(**{"destination":"/queue/a", "msg":"hello queue a"})
+        msg = "SEND\ndestination:/queue/a\n\nhello queue a\x00"
         self.assertEquals(self.decoder.decode(msg).__dict__, testFrame.__dict__)
 
+    def test_decoder_decode_send_missing_req_header(self):
+        msg = "SEND\n\nhello queue a\x00"
+        self.assertRaises(Exception, self.decoder.decode(msg))
 
 if __name__ == '__main__':
     unittest.main()
